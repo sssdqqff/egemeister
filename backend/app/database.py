65096d431aedaaf66a82d04962801c9b8dd9ae1b
Maxim <newbiestorm@icloud.com>
2026-07-18 10:54:14 +0400

    config

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import declarative_base
-from .config import settings
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine, text, URL
+from config import settings
 
-engine = create_async_engine(
-    settings.database_url,
-    pool_pre_ping=True,
+engine = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    echo=True,
+    pool_size=5,
+    max_overflow=10,
 )
 
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine,
-    expire_on_commit=False
-)
-
-Base = declarative_base()
+with engine.connect() as connection:
+    res = connection.execute(text("SELECT VERSION()"))
+    print(res)
